9c819d6d11bf97306a14d45856bde4cd9fc4bd9b
knee3h <knee3h@126.com>
2024-04-14 18:25:55 +0800

    删除gson依赖

--- src/main/java/com/mmall/practice/example/mq/kafka/KafkaSender.java
+++ src/main/java/com/mmall/practice/example/mq/kafka/KafkaSender.java
@@ -1,9 +1,9 @@
 package com.mmall.practice.example.mq.kafka;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.alibaba.fastjson2.JSON;
 import com.mmall.practice.example.mq.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +17,6 @@ public class KafkaSender {
     @Resource
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    private Gson gson = new GsonBuilder().create();
 
     public void send(String msg) {
         Message message = new Message();
@@ -25,6 +24,6 @@ public class KafkaSender {
         message.setMsg(msg);
         message.setSendTime(new Date());
         log.info("send message:{}", message);
-        kafkaTemplate.send(TopicConstants.TEST, gson.toJson(message));
+        kafkaTemplate.send(TopicConstants.TEST, JSON.toJSONString(message));
     }
 }
